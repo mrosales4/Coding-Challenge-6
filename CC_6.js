20b82de33d15e90c5aaafa45d25a6d7195689a0e
Matthew A Rosales <matthewrosales@usf.edu>
2024-06-04 20:35:12 -0400

    Create Ball constructor with initial properties.

--- CC_6.js
+++ CC_6.js
@@ -3,3 +3,14 @@
 // HTML canvas setup
 const canvas = document.getElementById("myCanvas"); // HTML canvas setup
 const ctx = canvas.getContext("2d"); // webgl or webgpu for 3d
+
+// Ball constructor with correct properties
+class Ball {
+  constructor(x, y, radius, dx, dy, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = color;
+  }
